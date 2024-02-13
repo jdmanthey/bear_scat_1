711619c9f89d3ad6d1740aa48323b99ab2ebb22b
Joe Manthey <jdmanthey@gmail.com>
2024-02-13 14:07:56 -0600

    Create 00_setup.sh

---
+++ 00_setup.sh
@@ -0,0 +1,18 @@
+# define main working directory
+workdir=/lustre/scratch/jmanthey/02_bear_scat
+
+# make output directories
+cd ${workdir}
+
+mkdir 00_fastq
+mkdir 01_cleaned
+mkdir 01_bam_files
+mkdir 02_vcf
+mkdir 03_vcf
+mkdir 04_filtered_vcf
+mkdir 05_pca
+mkdir 06_window_stats
+mkdir 06_window_stats/windows
+mkdir 07_kinship
+mkdir 10_filter
+mkdir 12_filter_vcf
