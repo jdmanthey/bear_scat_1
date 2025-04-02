16c4d6248417400229f8d5223a73dc63ff3b80d6
Joe Manthey <jdmanthey@gmail.com>
2025-04-01 23:10:02 -0500

    Create 07_pca_catkinship.sh

---
+++ 07_pca_catkinship.sh
@@ -0,0 +1,86 @@
+source activate bcftools
+
+cd /lustre/scratch/jmanthey/19_bears/05_pca
+
+
+##########################################
+##########################################
+### cat all individual scaffolds together
+##########################################
+##########################################
+
+grep "^#" structure_JANIGQ010000097.1.recode.vcf > structure.vcf
+
+for i in $( ls structure_*.recode.vcf ); do grep -v "^#" $i >> structure.vcf; done
+
+
+##########################################
+##########################################
+### remove all individual scaffold files
+##########################################
+##########################################
+
+rm *recode*
+
+##########################################
+##########################################
+### thin for 5 kbp separation between SNPs for PCA
+##########################################
+##########################################
+
+vcftools --vcf structure.vcf --thin 5000 --recode --recode-INFO-all --out structure
+
+##########################################
+##########################################
+### run plink for pca
+##########################################
+##########################################
+
+# make chromosome map for this vcf
+grep -v "#" structure.vcf | cut -f 1 | uniq | awk '{print $0"\t"$0}' > chrom_map.txt
+
+#plink output format
+vcftools --vcf structure.vcf  --plink --chrom-map chrom_map.txt --out structure 
+
+# convert  with plink
+plink --file structure --recode12 --allow-extra-chr \
+--out structure_plink
+
+# run pca on dataset
+plink --file structure_plink --pca --allow-extra-chr \
+--out structure_plink_pca
+
+
+# combine kinship vcfs
+
+
+cd /lustre/scratch/jmanthey/19_bears/07_kinship
+
+
+##########################################
+##########################################
+### cat all individual scaffolds together
+##########################################
+##########################################
+
+grep "^#" kinship_JANIGQ010000097.1.recode.vcf > kinship.vcf
+
+for i in $( ls kinship_*.recode.vcf ); do grep -v "^#" $i >> kinship.vcf; done
+
+
+##########################################
+##########################################
+### remove all individual scaffold files
+##########################################
+##########################################
+
+rm *recode*
+
+##########################################
+##########################################
+### thin for 5 kbp separation between SNPs for kinship
+##########################################
+##########################################
+
+vcftools --vcf kinship.vcf --thin 5000 --recode --recode-INFO-all --out kinship
+
